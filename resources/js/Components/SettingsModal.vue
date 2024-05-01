536dc5f377cd4cdd53b7c28e408422db32235a57
Kristaps-Stals <kristapsstals1@gmail.com>
2024-05-01 14:17:18 +0300

    Added settings foundation
    - Added SettingsModal
    - Fixed overflow on "About Us" text
    - Tweaked css

---
+++ resources/js/Components/SettingsModal.vue
@@ -0,0 +1,45 @@
+<!-- :isOpen: true - component is shown; false - component is hidden -->
+<!-- @close: emits when settings tab should be closed -->
+<!-- Example: <SettingsModal :isOpen="settingsOpen" @close="settingsOpen=false"/> -->
+
+<template>
+    <Transition name="fade">
+        <div v-if="isOpen" class="absolute w-full h-full z-20">
+            <!-- background -->
+            <div @click="close" class="absolute w-full h-full opacity-50 bg-black"/>
+
+            <!-- Main Section -->
+            <div class="absolute bg-light border-2 rounded-lg left-1/2 top-1/2 anchor-center text-white" style="height:90%;width:55%">
+                <!-- close button -->
+                <button @click="close" class="absolute w-16 h-16 right-4 top-4 btn-red">
+                    <p class="text-mid text-4xl select-none font-extrabold">X</p>
+                </button>
+
+                <!-- Title -->
+                <div class="absolute h-24 w-full pointer-events-none">
+                    <p class="text-mid text-4xl font-bold">Settings</p>
+                </div>
+
+                <!-- TODO: Main Section Main Section :) -->
+            </div>
+        </div>
+    </Transition>
+</template>
+
+<script>
+export default {
+    props:['isOpen'],
+    emits:['close'],
+    methods: {
+        close(){
+            this.$emit('close');
+        },
+    },
+}
+</script>
+
+<style>
+    .fade-enter-from, .fade-leave-to {opacity:0%;}
+    .fade-enter-to, .fade-leave-from {opacity:100%;}
+    .fade-enter-active, .fade-leave-active {transition: all 0.15s;}
+</style>
--- resources/js/Pages/LandingPage.vue
+++ resources/js/Pages/LandingPage.vue
@@ -4,16 +4,17 @@
         <p class="absolute text-6xl text-white font-bold anchor-center left-1/2 select-none" style="top:10%">QuizChef</p>
         
         <!-- Settings -->
-        <button class="absolute w-16 h-16 right-5 top-5">
+        <button @click="settingsOpen=true" class="absolute w-16 h-16 right-5 top-5">
             <img class="select-none" src="../../../public/assets/img/SettingsCog.png" draggable="false">
         </button>
-
+        <SettingsModal :isOpen="settingsOpen" @close="settingsOpen=false"/>
+        
         <!-- About Us -->
         <div class="absolute w-1/2 about-us-closed about-us-transition left-1/4 pointer-events-none" style="height:35%" :class="{'about-us-open':aboutUsOpen}">
             <button @click="aboutUsOpen=!aboutUsOpen" class="absolute pointer-events-auto" style="height:28.5%;width:20%;left:40%">
-                <p class="absolute anchor-center top-1/2 left-1/2 text-white text-nowrap text-xl select-none">About Us</p>
+                <p class="text-mid text-white text-nowrap text-xl select-none">About Us</p>
             </button>
-            <div class="absolute bg-light border-2 rounded-lg w-full pointer-events-auto" style="height:71.5%;top:28.5%">
+            <div class="absolute bg-light border-2 rounded-lg w-full pointer-events-auto overflow-hidden" style="height:71.5%;top:28.5%">
                 <p class="absolute text-white left-1/2 anchor-center" style="top:40%;width:90%">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
                     sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
@@ -36,7 +37,7 @@
                 :href="route('_login')"
                 class="btn-green absolute landing-page-btn" style="top:35%;height:20%;width:75%"
             >
-                <p class="absolute anchor-center top-1/2 left-1/2">Log In</p>
+                <p class="text-mid">Log In</p>
             </Link>
 
             <p class="absolute text-3xl text-white font-bold anchor-center left-1/2 select-none w-full text-center" style="top:55%">OR</p>
@@ -46,7 +47,7 @@
                 :href="route('_register')"
                 class="btn-blue absolute landing-page-btn" style="top:75%;height:20%;width:75%"
             >
-                <p class="absolute anchor-center top-1/2 left-1/2">Register</p>
+                <p class="text-mid">Register</p>
             </Link>
         </div>
         <div v-else class="text-white">canLogin == false; implement a thing that takes you to dashboard</div>
@@ -64,7 +65,7 @@
             <Link
                 class="btn-green absolute landing-page-btn" style="top:75%;height:20%;width:75%"
             >
-                <p class="absolute anchor-center top-1/2 left-1/2">Cook! 👨‍🍳</p>
+                <p class="text-mid">Cook! 👨‍🍳</p>
             </Link>
         </div>
     </div>
@@ -72,10 +73,12 @@
 
 <script>
 import { Link } from '@inertiajs/vue3';
+import SettingsModal from '../Components/SettingsModal.vue';
 
 export default {
     components: {
         Link,
+        SettingsModal,
     },
 
     props: {
@@ -98,11 +101,12 @@ export default {
     data (){
         return {
             aboutUsOpen: false,
+            settingsOpen: false,
         }
     },
 }
 </script>
 
 <style>
-
+    
 </style>
